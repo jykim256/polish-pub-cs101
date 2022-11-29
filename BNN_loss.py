8821d4c50f7a533b5863de42e367c36f77782356
jykim256 <jykim256@users.noreply.github.com>
2022-11-29 10:51:54 -0800

    lne

--- BNN_loss.py
+++ BNN_loss.py
@@ -16,6 +16,17 @@ def laplacian_normalized_noexp(y_pred, y_true, show_parts=True):
     )
     return loss
 
+def laplacian_normalized_exp(y_pred, y_true, show_parts=True):
+    mean_true = y_true[:, :, :, 0]
+    mean_pred = y_pred[:, :, :, 0]
+    scale_pred = y_pred[:, :, :, 1]
+    if show_parts:
+        tf.print("top_loss", tf.math.reduce_mean(K.abs(mean_true - mean_pred)))
+        tf.print("bottom_loss", tf.math.reduce_mean(2 * K.exp(scale_pred)))
+        tf.print("coef_loss", tf.math.reduce_mean(tf.divide(scale_pred, 2)))
+    loss = tf.math.divide((K.abs(mean_true - mean_pred)), 2 * K.exp(scale_pred)) + tf.divide(scale_pred, 2)
+    return loss
+
 
 def laplacian_denormalized_noexp(y_pred, y_true, show_parts=True):
     mean_true = tf.math.scalar_mul(2**16, y_true[:, :, :, 0])
